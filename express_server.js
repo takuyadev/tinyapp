c057eed8a15a57df528adee62c12c3777effd86f
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-03-20 12:48:30 -0700

    Working URL Routing

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -24,6 +24,14 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {
+    longURL: urlDatabase[req.params.id],
+    id: req.params.id,
+  };
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
