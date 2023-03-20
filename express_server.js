44e643c300bb7b8979a39fb7b30debdab7255554
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-03-20 13:49:59 -0700

    Fixed generateRandom Bug

--- express_server.js
+++ express_server.js
@@ -2,25 +2,29 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".split("")
+const chars =
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".split("");
 
 const generateRandomString = (arr, length) => {
-  const getRandomIndex = (arr) => Math.floor(Math.random * arr.length);
-
+  const getRandomIndex = (arr) => Math.floor(Math.random() * arr.length);
   let result = [];
+
   for (let i = 0; i < length; i++) {
-    const randommIndex = getRandomIndex(arr);
+    const randomIndex = getRandomIndex(arr);
     result.push(arr[randomIndex]);
   }
-  return result;
-};
 
-app.set("view engine", "ejs");
+  return result.join("");
+};
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -52,7 +56,9 @@ app.get("/urls/:id", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
+  const randomString = generateRandomString(chars, 6);
+  urlDatabase[randomString] = req.body.longURL;
+  console.log(urlDatabase)
   res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
