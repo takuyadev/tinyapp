8322a3a8bc8427538ad1f66cb4c48c4d7ef9f0e9
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-03-21 11:20:57 -0700

    Add login and cookie auth

--- express_server.js
+++ express_server.js
@@ -1,15 +1,15 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = 8080; // default port 8080
 
 // Constants
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
+  b2xVn2: 'http://www.lighthouselabs.ca',
+  '9sm5xK': 'http://www.google.com',
 };
 
 const chars =
-  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".split("");
+  'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'.split('');
 
 // Generates random number based on range
 const getRandomNumber = (range) => Math.floor(Math.random() * range);
@@ -19,59 +19,62 @@ const generateRandomString = (arr, length) => {
   return new Array(length).fill(0).reduce((acc) => {
     const randomIndex = getRandomNumber(arr.length);
     return (acc += arr[randomIndex]);
-  }, "");
+  }, '');
 };
 
 app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 // @route /urls
 // @desc Allow users to shorten their long URLs into shortened links
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.send("<html>Go to <a href='urls'>/urls</a></html>\n");
 });
 
-app.get("/urls", (req, res) => {
+app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+  res.render('urls_index', templateVars);
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
 });
 
-app.get("/urls/:id", (req, res) => {
+app.get('/urls/:id', (req, res) => {
   const templateVars = {
     longURL: urlDatabase[req.params.id],
     id: req.params.id,
   };
-  res.render("urls_show", templateVars);
+  res.render('urls_show', templateVars);
 });
 
-app.post("/urls", (req, res) => {
+app.post('/urls', (req, res) => {
   const randomString = generateRandomString(chars, 6);
   urlDatabase[randomString] = req.body.longURL;
   console.log(urlDatabase);
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  res.send('Ok'); // Respond with 'Ok' (we will replace this)
 });
 
-app.get("/u/:id", (req, res) => {
+app.get('/u/:id', (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
 });
 
-
-app.post("/urls/:id/delete", (req, res) => {
+app.post('/urls/:id/delete', (req, res) => {
   delete urlDatabase[req.params.id];
-  res.send("Deleted")
+  res.send('Deleted');
 });
 
-app.post("/urls/:id/edit", (req, res) => {
+app.post('/urls/:id/edit', (req, res) => {
   urlDatabase[req.params.id] = req.body.longURL;
-  res.send("Edit")
+  res.send('Edit');
 });
-
+app.post('/login', (req, res) => {
+  console.log(req.body)
+  res.cookie("username", req.body.username, { httpOnly: true });
+  res.send('Cookies');
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
